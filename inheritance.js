83ce41a9798c8dc175eebb541775e982d0a7706c
Faith <fmugure@intracitygeeks.org>
2017-12-12 01:16:05 +0000

    inheritance

---
+++ inheritance.js
@@ -0,0 +1,14 @@
+var john = {
+    name: "John",
+    yearOfBirth: 1990,
+    job: "teacher"
+};
+
+var Person = function(name, yearOfBirth, job){
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+    this.calculateAge = function(){
+        console.log(2016 - this.yearOfBirth);
+    }
+}
