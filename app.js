7c7b021c130105178731d5ca8d8d71f4566f5d88
Faith <fmugure@intracitygeeks.org>
2017-12-12 01:05:23 +0000

    pig game

--- app.js
+++ app.js
@@ -14,3 +14,10 @@ dice = Math.floor(Math.random() * 6) + 1;
 console.log(dice);
 
 document.querySelector("#current-" + activePlayer).textContent = dice;
+
+var x = document.querySelector("#score-0").textContent;
+console.log(x);
+
+document.querySelector(".dice").style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener("click")
