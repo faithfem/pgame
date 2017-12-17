20967aacfd48dedf332b3e54057f59ed44884316
Faith <fmugure@intracitygeeks.org>
2017-12-17 19:36:06 +0000

    Lec42 DRY principle & update HOLD function

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ document.querySelector(".btn-roll").addEventListener("click", function(){
     
     //2. Display the result
     //first bring back the view of the dice if you have it blocked (none)
-    //document.querySelector("dice").style.display="block"; //MINE IS NOT BLOCKED SO NO NEED FOR THIS LINE
+    //document.querySelector("dice").style.display="block"; 
     var diceDOM = document.querySelector(".dice");
     diceDOM.style.display = "block";
     diceDOM.src = "dice-" + dice + '.png';
@@ -72,30 +72,69 @@ document.querySelector(".btn-roll").addEventListener("click", function(){
 
     } else{
         //Next player's turn
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScoreCurrent = 0; //when it goes to next player, their score resets to zero
+        
+        //WE CAN USE THE CODE BELOW CALLED "//CODE CHANGED-SEE FUNCTION"IN LINES 77 TO 92 BUT WE ENDED UP CREATING A FUNCTION (LINES 117-126) AND WE'LL USE THAT INSTEAD
+        //CODE CHANGED-SEE FUNCTION: activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        //CODE CHANGED-SEE FUNCTION:roundScoreCurrent = 0; //when it goes to next player, their score resets to zero
     
-        document.getElementById("roundScoreCurrent-0").textContent = "0"; //score resets to zero
-        document.getElementById("roundScoreCurrent-1").textContent = "0"; //score resets to zero
+        //CODE CHANGED-SEE FUNCTION: document.getElementById("roundScoreCurrent-0").textContent = "0"; //score resets to zero
+        //CODE CHANGED-SEE FUNCTION:document.getElementById("roundScoreCurrent-1").textContent = "0"; //score resets to zero
         
         //WHEN GAME MOVES TO NEXT PLAYER, THE PANEL AND ALL INDICATORS CHANGE TO SHOW WHO IS THE ACTIVE PLAYER NOW
-        //METHOD BELOW (LINES 86-87) IS OKAY, BUT NOT SUFFICIENT. BETTER TO USE TOGGLE METHOD - STARTS IN LINE 89
+        //METHOD BELOW IS OKAY, BUT NOT SUFFICIENT. BETTER TO USE TOGGLE METHOD - STARTS IN LINE 91
         //i) select document by docu query selec method
         //ii) select the player - html line 14
         //iii) mark the new player as active. Active indicators are dictated by css styles, line 73 to 82.
         //document.querySelector(".player-0-panel").classList.remove("active"); //When first player gets a zero, remove him from being the active player
         //document.querySelector(".player-1-panel").classList.add("active");
         
-        document.querySelector(".player-0-panel").classList.toggle("active"); 
-        document.querySelector(".player-1-panel").classList.toggle("active");
+        //CODE CHANGED-SEE FUNCTION:document.querySelector(".player-0-panel").classList.toggle("active"); 
+        //CODE CHANGED-SEE FUNCTION:document.querySelector(".player-1-panel").classList.toggle("active");
         
         //LET'S HIDE THE DICE WHEN PLAYER ROLLS A 1 (EASIER ON THE EYES, TRUST ME - ACTUALLY, I DON'T REALLY LIKE THE HIDING, SO I'LL COMMENT THE LINE BELOW OUT)
         //document.querySelector(".dice").style.display = "none";
+        
+        nextPlayer(); //USING THE FUNCTION IN LINES 117-126
+    }
+});
+
+//HOLDING AND TALLYING UP THE GLOBAL SCORE
+//REMEMBER WHEN PLAYER HITS A ONE, IT'S THE NEXT PLAYER'S TURN. IF HE HOLDS, IT'S AGAIN THE NEXT PLAYER'S TURN. SO IN THE SPIRIT OF DRY (DON'T REPEAT YOURSELF) THIS IS THE SAME CODE
+document.querySelector(".btn-hold").addEventListener("click", function(){
+    //first you want to take the current round's score and add it to the global score
+    scores[activePlayer] += roundScoreCurrent;
     
+    //Update the user interface
+    document.querySelector("#globalScore-" + activePlayer).textContent = scores[activePlayer];
+    
+    //Check if the player won the game (wins if global score reaches 100 first)
+    if (scores[activePlayer] >= 20) {
+        document.querySelector("#name-" + activePlayer).textContent = "Winner!"; //targets the name of the winner and changes it to winner
+        document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+        document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+        console.log("Game over, next round" );
+    } else{
+        nextPlayer();
+        console.log("continue playing");
     }
     
-    
+    //NEXT PLAYER
+    //can copy lines 77 to 92 here, but that's repeating ourselves and since the code is the same, if we change lines 77-92, we have to come here & change these lines
+    //so best to create a function for the next player
+    nextPlayer(); //USING THE FUNCTION IN LINES 117-126 INSTEAD OF THE OLD CODE IN LINES 77-92 (THOSE LINES WERE MOVED TO THE FUNCTION SO IF WE HAD TO CHANGE ANYTHING, WE CHANGE IT IN THE FUNCTION ONLY INSTEAD OF IN MULTIPLE PLACES)
 });
+
+function nextPlayer(){
+    
+     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScoreCurrent = 0; //when it goes to next player, their score resets to zero
+    
+        document.getElementById("roundScoreCurrent-0").textContent = "0"; //score resets to zero
+        document.getElementById("roundScoreCurrent-1").textContent = "0"; //score resets to zero
+
+        document.querySelector(".player-0-panel").classList.toggle("active"); 
+        document.querySelector(".player-1-panel").classList.toggle("active");
+}
 
 
 
